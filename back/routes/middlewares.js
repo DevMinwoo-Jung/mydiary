4bec7dac7adab5c57cb50118d299ba5754638de5
DEVMINWOO-JUNG <applejung312@gmail.com>
2022-08-28 22:47:48 +0900

    sunday

--- back/routes/middlewares.js
+++ back/routes/middlewares.js
@@ -13,6 +13,6 @@ exports.isNotLoggedIn = (req, res, next) => {
         res.status(401).send('can access only who not login')
     }
 }
-
+ 
 
 // 지금 위의 것들을 여기서 하는게 아니라 route에서 해도 될 텐데 왜 따로 뺐을까?? -> 코드의 중복 제거
