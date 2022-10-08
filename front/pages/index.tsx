b08f6366cd54baafa3eda4ddbc1b21a10868df7e
DEVMINWOO-JUNG <applejung312@gmail.com>
2022-10-08 11:24:39 +0900

    minor change

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -65,7 +65,7 @@ const _index: NextPage = () => {
   const arrowRef: any = useRef()
 
   useLayoutEffect(() => {
-    if (me === null) {
+    if (me !== null) {
       dispatch({
         type: LOAD_MY_INFO_REQUEST,
       })
